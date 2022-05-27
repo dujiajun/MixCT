9ff8fbc0f3550fa3c2dda328113e58741d96ee08
Du Jiajun <jiajun_du@outlook.com>
2022-05-27 17:06:02 +0800

    test: test more primitives

--- test/TestPrimitives.sol
+++ test/TestPrimitives.sol
@@ -57,10 +57,68 @@ contract TestPrimitives {
         Assert.isTrue(c.eq(expected), "commitment is ok");
     }
 
+    function testMultiExp() public {
+        Utils.G1Point[] memory gs = new Utils.G1Point[](2);
+
+        gs[0] = Utils.G1Point(
+            0x14601b8cdf761d4ed94554865ef0ef5c451e275f3dfc0a667fea04fa5a833bed,
+            0x4b23a3c385114c40cb4fbf02d1a52f731b4edf61c247372d038470eea90edffb
+        );
+
+        gs[1] = Utils.G1Point(
+            0x6efee4d1ba231acfee2391dc5ded838cee89235af14b8a4f494e4734cb1323f5,
+            0x9f182dfa64bd4e92d1e00fdf100c28a3361860fda763f03d5cffb652097f006e
+        );
+
+        uint256[] memory x = new uint256[](2);
+        x[0] = 10;
+        x[1] = 20;
+
+        Utils.G1Point memory expected = Utils.G1Point(
+            0x88beb71b12893f92ace15836af060835e059333f18483915c533ca09aa455bcc,
+            0xa482c8f3768f85b95d4e147663574598649fbdf5cedcbc38b034e5ad12bd06ca
+        );
+
+        Utils.G1Point memory c = Primitives.multiExp(gs, x);
+
+        Assert.isTrue(c.eq(expected), "multiExp is ok");
+    }
+
+    function testCommitBits() public {
+        Utils.G1Point memory g = Utils.G1Point(
+            0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798,
+            0x483ada7726a3c4655da4fbfc0e1108a8fd17b448a68554199c47d08ffb10d4b8
+        );
+
+        Utils.G1Point[] memory hs = new Utils.G1Point[](2);
+        hs[0] = Utils.G1Point(
+            0xc4abbb41fb87d293ae90fd755c1e62506b7c80d2fe84efa36970383e17ca274a,
+            0xd730074791dbacb3bc866d4600b62d1da3bd1aacc2da289f20424036f10c1c06
+        );
+        hs[1] = Utils.G1Point(
+            0xd67dedde7f8861e5a99c0e30e06594997e85da6604ceffd429c69bf9d1d5b4d7,
+            0x77f0f57c3757fc327265bf588cf1ddef2ca35b1445e9374e44ca710301bd9b61
+        );
+
+        uint256[] memory exps = new uint256[](2);
+        exps[0] = 10;
+        exps[1] = 20;
+        uint256 r = 20;
+
+        Utils.G1Point memory expected = Utils.G1Point(
+            0xce3ee0219f20d53c1b45d8d3e8fb638af4b180c4a8b4467097ccb489a2d0d603,
+            0xd54618af30dfe5a24f516a6b57809926c618fb0cb6fe6431176b732e4c1e1c0c
+        );
+
+        Utils.G1Point memory c = Primitives.commitBits(g, hs, exps, r);
+
+        Assert.isTrue(c.eq(expected), "commitBits is ok");
+    }
+
     function testConvertToNal() public {
         uint256 n = 4;
         uint256 m = 2;
-        for (uint256 index = 0; index < n ** m; index++) {
+        for (uint256 index = 0; index < n**m; index++) {
             uint256[] memory I = Primitives.convertToNal(index, n, m);
             Assert.equal(I[0] + I[1] * n, index, "convertToNal OK");
         }
